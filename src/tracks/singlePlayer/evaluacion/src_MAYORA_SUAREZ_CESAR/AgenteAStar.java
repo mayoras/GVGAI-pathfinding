78fe4d835c7ed2c5da8a5fccc9d884d45d4b9b25
Cesar Mayora <cesarmay@protonmail.com>
2023-04-07 00:53:46 +0200

    A* algorithm passing tests - still slower than Dijkstra - >3ms in map 8

--- src/tracks/singlePlayer/evaluacion/src_MAYORA_SUAREZ_CESAR/AgenteAStar.java
+++ src/tracks/singlePlayer/evaluacion/src_MAYORA_SUAREZ_CESAR/AgenteAStar.java
@@ -37,6 +37,10 @@ public class AgenteAStar extends AbstractPlayer {
     // Map's number of cells per row and column
     int nx, ny;
 
+    boolean[][] visited;
+
+    public static ACTIONS[] EXPANDED_ACTIONS = {ACTIONS.ACTION_UP, ACTIONS.ACTION_DOWN, ACTIONS.ACTION_LEFT, ACTIONS.ACTION_RIGHT};
+
     public AgenteAStar(StateObservation stateObs, ElapsedCpuTimer elapsedTimer) {
         this.scaleF = new Vector2d((float)stateObs.getWorldDimension().width / stateObs.getObservationGrid().length,
                 (float)stateObs.getWorldDimension().height / stateObs.getObservationGrid()[0].length);
@@ -48,6 +52,9 @@ public class AgenteAStar extends AbstractPlayer {
         this.invalid = new boolean[this.nx][this.ny];
         this.g = new int[this.nx][this.ny];
         this.parent = new Vector2d[this.nx][this.ny];
+
+        // Default declaration value is false
+        this.visited = new boolean[this.nx][this.ny];
 
         // Initialize distance/cost matrix
         for (int i = 0; i < this.nx; ++i) {
@@ -94,79 +101,125 @@ public class AgenteAStar extends AbstractPlayer {
                                         Math.floor(stateObs.getAvatarPosition().y / scaleF.y));
         this.g[(int)avatar.x][(int)avatar.y] = 0;
 
-        int curr_x, curr_y;
+        int currX, currY;
         ArrayList<Vector2d> frontier = new ArrayList<>(this.nx * this.ny);
         // add starting node to frontier list
         frontier.add(avatar);
+
+        Vector2d next = new Vector2d();
 
         long start = System.nanoTime();
         while (true) {
             // get curr node, as the one with minimum cost of frontier nodes
             int minDist = Integer.MAX_VALUE;
-            curr_x = -1;
-            curr_y = -1;
+            currX = -1;
+            currY = -1;
             Vector2d curr = null;
-            for (Vector2d f : frontier) {
-                // if the cost of node is minimum and its position is valid
-                if (minDist > this.g[(int)f.x][(int)f.y] && !this.invalid[(int)f.x][(int)f.y]) {
-                    minDist = this.g[(int)f.x][(int)f.y];
-                    curr_x = (int)f.x;
-                    curr_y = (int)f.y;
-                    curr = f;
+            for (Vector2d f_node : frontier) {
+                // if the f=g+h heuristic value of node is minimum and its position is valid
+                if (minDist > f((int)f_node.x, (int)f_node.y) && !this.invalid[(int)f_node.x][(int)f_node.y]) {
+                    minDist = this.g[(int)f_node.x][(int)f_node.y];
+                    currX = (int)f_node.x;
+                    currY = (int)f_node.y;
+                    curr = f_node;
                 }
             }
 
             // check if the current position is goal's
-            if (curr_x == this.portal.x && curr_y == this.portal.y) {
+            if (currX == this.portal.x && currY == this.portal.y) {
                 break;
             }
 
-            // node is visited, invalid position for not going backwards
-            this.invalid[curr_x][curr_y] = true;
+            // node is visited, therefore an invalid position for not going backwards
+            this.visited[currX][currY] = true;
             frontier.remove(curr);
 
-            // Expand node
-            Vector2d parent = new Vector2d(curr_x, curr_y);
-            // up
-            if (!this.invalid[curr_x][curr_y - 1] && this.g[curr_x][curr_y - 1] > minDist + 1) {
-                this.g[curr_x][curr_y - 1] = minDist + 1;
-                this.parent[curr_x][curr_y - 1] = parent;
-                frontier.add(new Vector2d(curr_x, curr_y - 1));
-                ++this.expandedNodes;
-            }
-            // down
-            if (!this.invalid[curr_x][curr_y + 1] && this.g[curr_x][curr_y + 1] > minDist + 1) {
-                this.g[curr_x][curr_y + 1] = minDist + 1;
-                this.parent[curr_x][curr_y + 1] = parent;
-                frontier.add(new Vector2d(curr_x, curr_y + 1));
-                ++this.expandedNodes;
-            }
-            // left
-            if (!this.invalid[curr_x - 1][curr_y] && this.g[curr_x - 1][curr_y] > minDist + 1) {
-                this.g[curr_x - 1][curr_y] = minDist + 1;
-                this.parent[curr_x - 1][curr_y] = parent;
-                frontier.add(new Vector2d(curr_x - 1, curr_y));
-                ++this.expandedNodes;
-            }
-            // right
-            if (!this.invalid[curr_x + 1][curr_y] && this.g[curr_x + 1][curr_y] > minDist + 1) {
-                this.g[curr_x + 1][curr_y] = minDist + 1;
-                this.parent[curr_x + 1][curr_y] = parent;
-                frontier.add(new Vector2d(curr_x + 1, curr_y));
-                ++this.expandedNodes;
+            //////////// Expand node //////////
+
+            // save the position of parent
+            Vector2d parent = new Vector2d(currX, currY);
+
+            for (ACTIONS action : AgenteAStar.EXPANDED_ACTIONS) {
+                nextSuccessor(action, next, currX, currY);
+
+                int nextX = (int)next.x;
+                int nextY = (int)next.y;
+
+                // if it's a wall or trap, don't expand
+                if (this.invalid[nextX][nextY]) continue;
+
+                if (
+                        // it's been visited and less cost, we found a better path to get this node
+                        this.visited[nextX][nextY]
+                        && this.g[nextX][nextY] > minDist + 1
+                ) {
+                    this.visited[nextX][nextY] = false;
+                    frontier.add(next);
+                    this.parent[nextX][nextY] = parent;
+                    this.g[nextX][nextY] = minDist + 1;
+                } else if (
+                        // it's been not visited and it's not frontier, expand node
+                        !this.visited[nextX][nextY]
+                        && !frontier.contains(next)
+                ) {
+                    frontier.add(next.copy());
+                    this.parent[nextX][nextY] = parent;
+                    this.g[nextX][nextY] = minDist + 1;
+                } else if (
+                        // it's frontier and the cost to get to it is less, update its cost
+                        frontier.contains(next.copy())
+                        && this.g[nextX][nextY] > minDist + 1
+                ) {
+                    this.g[nextX][nextY] = minDist + 1;
+                }
+
+//                if (this.visited[(int)next.x][(int)next.y] && this.g[(int)next.x][(int)next.y] > minDist + 1) {
+//                    this.invalid[(int)next.x][(int)next.y] = false;
+//                    this.g[(int)next.x][(int)next.y] = minDist + 1;
+//                    this.parent[(int)next.x][(int)next.y] = parent;
+//                    frontier.add(next);
+//                    ++this.expandedNodes;
+//                } else if (!this.invalid[(int)next.x][(int)next.y] && !frontier.contains(next)) {
+//                    frontier.add(next);
+//                    ++this.expandedNodes;
+//                } else if (frontier.contains(next) && this.g[(int)next.x][(int)next.y] > minDist + 1) {
+//                    this.g[(int)next.x][(int)next.y] = minDist + 1;
+//                }
             }
         }
         long end = System.nanoTime();
 
-        System.out.println("Runtime - Dijkstra: " + (end - start) / 1e6 + " ms");
+        System.out.println("Runtime - AStar : " + (end - start) / 1e6 + " ms");
         System.out.println("Nodes expanded: " + this.expandedNodes);
 
         // Reconstruct path
-        rebuildPath(curr_x, curr_y);
+        rebuildPath(currX, currY);
 
         System.out.println("Computed path length: " + this.plan.size());
 
         return this.plan.pop();
+    }
+
+    private void nextSuccessor(ACTIONS action, Vector2d next, int currX, int currY) {
+        switch (action) {
+            case ACTION_UP -> {
+                next.x = currX;
+                next.y = currY - 1;
+            }
+            case ACTION_DOWN -> {
+                next.x = currX;
+                next.y = currY + 1;
+            }
+            case ACTION_LEFT -> {
+                next.x = currX - 1;
+                next.y = currY;
+            }
+            case ACTION_RIGHT -> {
+                next.x = currX + 1;
+                next.y = currY;
+            }
+            default -> {}
+        }
     }
 
     /**
@@ -203,5 +256,10 @@ public class AgenteAStar extends AbstractPlayer {
             curr_x = (int)parent.x;
             curr_y = (int)parent.y;
         }
+    }
+
+    private int f(int x, int y) {
+        return this.g[x][y] +
+               (int)(Math.abs(x - this.portal.x) + Math.abs(y - this.portal.y));
     }
 }
